0b78f7d0eea141c456652c4003c21cfa787dbb72
PixelCat <65646483+PixelCat31415@users.noreply.github.com>
2022-01-04 01:09:31 +0800

    set up the project

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile(path.join(__dirname, "index.html"));
+};
+
+app.on("ready", createWindow);
+
+// make maxOS happy
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
